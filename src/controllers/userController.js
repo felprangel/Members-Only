0d49b7611af78e47fa483d3d394ca022647b0b00
felprangel <feliperangel897@gmail.com>
2024-10-15 21:31:44 -0300

    remove validações

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,13 +2,7 @@ const db = require("../db/queries");
 
 function createUser(req, res) {
   const { username, password } = req.body;
-  db.createUser(username, password)
-    .then(() => {
-      res.status(201).json({ message: "User created" });
-    })
-    .catch((err) => {
-      res.status(400).json({ message: err.message });
-    });
+  db.createUser(username, password);
 }
 
 module.exports = {
