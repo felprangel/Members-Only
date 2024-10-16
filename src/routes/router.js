69e9dd58b928431a02e068cc30eafda83702e4b9
felprangel <feliperangel897@gmail.com>
2024-10-15 21:23:16 -0300

    adiciona rota para cadastro

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,9 +1,12 @@
 const { Router } = require("express");
+const userController = require("../controllers/userController");
 
 const router = Router();
 
 router.get("/", async (req, res) => {
   res.render("index");
 });
+
+router.post("/sign-up", userController.createUser);
 
 module.exports = router;
