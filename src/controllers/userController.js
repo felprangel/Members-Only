e00e031698c76e76246034ab49fa63ed96bb8d3f
felprangel <feliperangel897@gmail.com>
2024-10-19 11:56:57 -0300

    adiciona funções do passport

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,4 @@
+const passport = require("passport");
 const db = require("../db/queries");
 
 async function createUser(req, res) {
@@ -22,6 +23,39 @@ function createMessage(req, res) {
   const { title, messageBody } = req.body;
   db.createMessage(title, messageBody);
 }
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = db.getUserByUsername(username);
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = db.getUserById(id);
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
 
 module.exports = {
   createUser,
