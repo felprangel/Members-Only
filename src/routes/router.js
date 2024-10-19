066ef1c3131bd6c70aa86c860e40f50b12643608
felprangel <feliperangel897@gmail.com>
2024-10-19 11:17:40 -0300

    adiciona rota para renderizar formulario

--- src/routes/router.js
+++ src/routes/router.js
@@ -6,6 +6,9 @@ const router = Router();
 router.get("/", async (req, res) => {
   res.render("index");
 });
+router.get("/sign-up", async (req, res) => {
+  res.render("sign-up-form");
+});
 
 router.post("/sign-up", userController.createUser);
 router.post("/message", userController.createMessage);
